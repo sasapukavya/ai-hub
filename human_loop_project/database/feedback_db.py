d9fb3a70458bbfed31e3cdadf8914118b9fe09ac
sasapukavya <sasapukavya5@gmail.com>
2026-09-07 11:14:19 +0530

    Update COSMOS_KEY retrieval method

--- human_loop_project/database/feedback_db.py
+++ human_loop_project/database/feedback_db.py
@@ -18,7 +18,7 @@ COSMOS_URL = os.getenv(
 
 COSMOS_KEY = os.getenv(
     "COSMOS_KEY",
-    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+    "C2y---------------------------------------------------------------------"
 )
 
 client = CosmosClient(
